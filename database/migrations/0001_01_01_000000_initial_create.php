1161f4e6567940ffd48c3f889f3177610014535c
manuelpt1993 <manuelpt1993@gmail.com>
2024-05-13 18:41:54 +0200

    minor

--- database/migrations/0001_01_01_000000_initial_create.php
+++ database/migrations/0001_01_01_000000_initial_create.php
@@ -67,20 +67,7 @@ return new class extends Migration
             $table->foreign('languageId')->references('id')->on('languages');
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
         // fine utenti
 
         // cache
@@ -97,40 +84,7 @@ return new class extends Migration
         });
         // fine cache
 
-        // jobs
-        Schema::create('jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('queue')->index();
-            $table->longText('payload');
-            $table->unsignedTinyInteger('attempts');
-            $table->unsignedInteger('reserved_at')->nullable();
-            $table->unsignedInteger('available_at');
-            $table->unsignedInteger('created_at');
-        });
-
-        Schema::create('job_batches', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->string('name');
-            $table->integer('total_jobs');
-            $table->integer('pending_jobs');
-            $table->integer('failed_jobs');
-            $table->longText('failed_job_ids');
-            $table->mediumText('options')->nullable();
-            $table->integer('cancelled_at')->nullable();
-            $table->integer('created_at');
-            $table->integer('finished_at')->nullable();
-        });
-
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
-        });
-        // fine jobs
+        
 
         // pat
         Schema::create('personal_access_tokens', function (Blueprint $table) {
@@ -235,18 +189,13 @@ return new class extends Migration
     {
         // utenti
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        
 
         // cache
-        Schema::dropIfExists('cache');
-        Schema::dropIfExists('cache_locks');
+         Schema::dropIfExists('cache');
+         Schema::dropIfExists('cache_locks');
 
-        // jobs
-        Schema::dropIfExists('jobs');
-        Schema::dropIfExists('job_batches');
-        Schema::dropIfExists('failed_jobs');
-
+        
         // pat
         Schema::dropIfExists('personal_access_tokens');
 
